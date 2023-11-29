193c666825a5eb18a3fac42bd47bb4dd7f328727
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2023-11-29 17:48:51 +0600

    Add Array sort

--- main.js
+++ main.js
@@ -1823,8 +1823,239 @@
 // });
 // document.querySelector(".demo").innerHTML = "Lowest Number = " + points[0];
 
-const points = [40, 100, 1, 5, 25, 10];
-points.sort(function (a, b) {
-  return a - b;
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function (a, b) {
+//   return a - b;
+// });
+// document.querySelector(".demo1").innerHTML = points[2];
+
+// let digits = [50, 30, 10, 40, 20];
+// digits.sort(function (a, b) {
+//   return b - a;
+// });
+// document.querySelector(".demo2").innerHTML = digits[0];
+
+// let digit = [100, 60, 80, 70, 90];
+// digit.sort(function (a, b) {
+//   console.log("Function");
+//   return b - a;
+// });
+// document.querySelector(".demo3").innerHTML = "Digit 0 : " + digit[0];
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo1").innerHTML = myArrayMax(points);
+
+// function myArrayMax(arr) {
+//   return Math.max.apply(null, arr);
+// }
+
+// const digits = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo2").innerHTML = ArrayMax(digits);
+// function ArrayMax(arr) {
+//   return Math.max.apply(null, arr);
+// }
+
+// let baaa = "b" + "a" + +"a" + "a";
+// console.log(baaa.toUpperCase());
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo1").innerHTML = "Min digit- " + myArrayMin(points);
+
+// function myArrayMin(arr) {
+//   return Math.min.apply(null, arr);
+// }
+
+// const point = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo2").innerHTML = "Max digit-" + myArrayMax(point);
+// function myArrayMax(arr) {
+//   return Math.max.apply(null, arr);
+// }
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo1").innerHTML =
+//   "Array Max Value: " + myArrayMax(points);
+
+// function myArrayMax(arr) {
+//   let len = arr.length;
+//   let max = -Infinity;
+//   while (len--) {
+//     if (arr[len] > max) {
+//       max = arr[len];
+//     }
+//   }
+//   return max;
+// }
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo1").innerHTML = myArrayMax(points);
+
+// function myArrayMax(arr) {
+//   let len = arr.length;
+//   let max = -Infinity;
+//   while (len--) {
+//     if (arr[len] > max) {
+//       max = arr[len];
+//     }
+//   }
+//   return max;
+// }
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.querySelector(".demo1").innerHTML = myArrayMin(points);
+
+// function myArrayMin(arr) {
+//   let len = arr.length;
+//   let min = Infinity;
+//   while (len--) {
+//     if (arr[len] < min) {
+//       min = arr[len];
+//     }
+//   }
+//   return min;
+// }
+
+// const cars = [
+//   { type: "Volvo", year: 2016 },
+//   { type: "Saab", year: 2001 },
+//   { type: "BMW", year: 2010 },
+// ];
+// displayCars();
+
+// cars.sort(function (a, b) {
+//   return a.year - b.year;
+// });
+// displayCars();
+
+// function displayCars() {
+//   document.querySelector(".demo1").innerHTML =
+//     cars[0].type +
+//     " " +
+//     cars[0].year +
+//     "<br>" +
+//     cars[1].type +
+//     " " +
+//     cars[1].year +
+//     "<br>" +
+//     cars[2].type +
+//     " " +
+//     cars[2].year;
+// }
+
+// const cars = [
+//   { type: "Volvo", year: 2016 },
+//   { type: "Saab", year: 2001 },
+//   { type: "BMW", year: 2010 },
+// ];
+
+// displayCars();
+
+// function myFunction1() {
+//   console.log("MyFunction");
+//   cars.sort(function (a, b) {
+//     let x = a.type.toLowerCase();
+//     let y = b.type.toLowerCase();
+//     if (x < y) {
+//       return -1;
+//     }
+//     if (x > y) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   displayCars();
+// }
+
+// function displayCars() {
+//   console.log("DisplayCars");
+//   document.querySelector(".demo1").innerHTML =
+//     cars[0].type +
+//     " " +
+//     cars[0].year +
+//     "<br>" +
+//     cars[1].type +
+//     " " +
+//     cars[1].year +
+//     "<br>" +
+//     cars[2].type +
+//     " " +
+//     cars[2].year;
+// }
+
+// const cars = [
+//   { type: "Volvo", year: 2016 },
+//   { type: "Saab", year: 2001 },
+//   { type: "BMW", year: 2010 },
+// ];
+
+// displayCars();
+
+// function myFunction1() {
+//   console.log("MyFunction");
+//   cars.sort(function (a, b) {
+//     let x = a.type.toLowerCase();
+//     let y = b.type.toLowerCase();
+//     if (x < y) {
+//       return -1;
+//     }
+//     if (x > y) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   displayCars();
+// }
+
+// function displayCars() {
+//   console.log("displayCars");
+//   document.querySelector(".demo1").innerHTML =
+//     cars[0].type +
+//     " " +
+//     cars[0].year +
+//     "<br>" +
+//     cars[1].type +
+//     " " +
+//     cars[1].year +
+//     "<br>" +
+//     cars[2].type +
+//     " " +
+//     cars[2].year;
+// }
+
+const myArr = [
+  { name: "X00", price: 100 },
+  { name: "X01", price: 100 },
+  { name: "X02", price: 100 },
+  { name: "X03", price: 100 },
+  { name: "X04", price: 110 },
+  { name: "X05", price: 110 },
+  { name: "X06", price: 110 },
+  { name: "X07", price: 110 },
+  { name: "X08", price: 120 },
+  { name: "X09", price: 120 },
+  { name: "X10", price: 120 },
+  { name: "X11", price: 120 },
+  { name: "X12", price: 130 },
+  { name: "X13", price: 130 },
+  { name: "X14", price: 130 },
+  { name: "X15", price: 130 },
+  { name: "X16", price: 140 },
+  { name: "X17", price: 140 },
+  { name: "X18", price: 140 },
+  { name: "X19", price: 140 },
+];
+
+myArr.sort((p1, p2) => {
+  if (p1.price < p2.price) return -1;
+  if (p1.price > p2.price) return 1;
+  return 0;
 });
-document.querySelector(".demo1").innerHTML = points[0];
+
+let text = "";
+myArr.forEach(myFunction1);
+
+function myFunction1(value) {
+  console.log("Function");
+  text += value.name + " " + value.price + "<br>";
+}
+
+document.querySelector(".demo1").innerHTML = text;
